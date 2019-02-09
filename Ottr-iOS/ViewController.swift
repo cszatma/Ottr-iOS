1e73d04b517c10693d89aad58a0abe54f84eb55c
Christopher Szatmary <christopherszatmary@gmail.com>
2019-02-09 11:54:50 -0500

    Initial Commit

---
+++ Ottr-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ottr-iOS
+//
+//  Created by Christopher Szatmary on 2019-02-09.
+//  Copyright © 2019 Christopher Szatmary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
