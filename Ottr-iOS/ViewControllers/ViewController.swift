38d1b26359fedd1844ad5cd62613942395185634
Christopher Szatmary <christopherszatmary@gmail.com>
2019-02-10 08:48:30 -0500

    The server requests the current user when the app loads

--- Ottr-iOS/ViewControllers/ViewController.swift
+++ Ottr-iOS/ViewControllers/ViewController.swift
@@ -8,78 +8,52 @@
 
 import UIKit
 import Alamofire
+import ReSwift
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, StoreSubscriber {
+    func newState(state: AppState) {
+        print(state.userState.currentUser)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        store.subscribe(self)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        store.unsubscribe(self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        store.dispatch(fetchUser)
         // Do any additional setup after loading the view, typically from a nib.
         let url = "http://localhost:8000"
         // Do any additional setup after loading the view, typically from a nib.
-        //        Alamofire.request("http://localhost:8000/test").responseJSON { response in
-        //                guard response.result.error == nil else {
-        //                    print(response.result.error!)
-        //                    return
-        //                }
-        //
-        //                guard let json = response.result.value as? [String: Any] else {
-        //                    print("didn't get todo object as JSON from API")
-        //                    if let error = response.result.error {
-        //                        print("Error: \(error)")
-        //                    }
-        //                    return
-        //                }
-        //
-        //                guard let name = json["name"] as? String else {
-        //                    print("Could not get name from JSON")
-        //                    return
-        //                }
-        //                print(name)
-        //                print(json)
-        //        }
-        
-        let parameters: Parameters = [
-            "email": "chris@email.com",
-            "password": "ottr"
-        ]
-        Alamofire.request("\(url)/api/auth/login", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
-            guard response.result.error == nil else {
-                print(response.result.error!)
-                return
-            }
-            
-            guard let json = response.result.value as? [String: Any] else {
-                print("didn't get todo object as JSON from API")
-                if let error = response.result.error {
-                    print("Error: \(error)")
-                }
-                return
-            }
-            
-            print(json)
-            
-            let cookies = NSHTTPCookie.cookiesWithResponseHeaderFields(response.response?.allHeaderFields as! [String: String], forURL: (response.response?.URL!)!)
-            
-            //Save method
-            setCookie(cookies.first!)
-            
-            Alamofire.request("\(url)/api/auth/current_user").responseJSON { response in
-                guard response.result.error == nil else {
-                    print(response.result.error!)
-                    return
-                }
-                
-                guard let json = response.result.value as? [String: Any] else {
-                    print("didn't get todo object as JSON from API")
-                    if let error = response.result.error {
-                        print("Error: \(error)")
-                    }
-                    return
-                }
-                
-                print(json)
-            }
-        }
+//            let cookies = HTTPCookie.cookies(withResponseHeaderFields: response.response?.allHeaderFields as! [String : String], for: (response.response?.url)!)
+//
+//            if let cookie = cookies.first {
+//                Cookie.set(cookie: cookie, forKey: Cookie.userSession)
+//            } else if let cookies = HTTPCookieStorage.shared.cookies {
+//                Cookie.set(cookie: cookies[0], forKey: Cookie.userSession)
+//            }
+//
+//            Alamofire.request("\(url)/api/auth/current_user").responseJSON { response in
+//                guard response.result.error == nil else {
+//                    print(response.result.error!)
+//                    return
+//                }
+//
+//                guard let json = response.result.value as? [String: Any] else {
+//                    print("didn't get todo object as JSON from API")
+//                    if let error = response.result.error {
+//                        print("Error: \(error)")
+//                    }
+//                    return
+//                }
+//
+//                print(json)
+//            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
